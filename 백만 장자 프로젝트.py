7b4a2b4c759fd6db3f7a5a88d9a89dd1a32490e6
yujin <yujin1203713@gmail.com>
2021-07-16 17:11:07 +0900

    incomplete

--- 백만 장자 프로젝트.py
+++ 백만 장자 프로젝트.py
@@ -1,18 +1,16 @@
-test_num = int(input("며칠?:"))
+test_num = int(input())
 
 for i in range(test_num):
     day_num = int(input())
     day_cost = list(map(int, input().split()))
 
-    stack = [day_cost[0]]  # 첫날 가격 미리 넣어둠
     revenue = 0
+    max = day_cost[-1]  # 뒤에서부터 풀기! 큰값을 계속 갱신, 작으면 사서 큰값으로 팔음
 
-    for j in range(1, day_num):
-        if(day_cost[j] >= stack[-1]):  # 오늘이 어제보다 비쌀 경우
-            stack.append(day_cost[j])
-        else:  # 오늘이 어제보다 쌀 경우 (-->어제 가격으로 다 팔아야 함)
-            sell_cost = stack.pop()
-            while(len(stack) != 0):  # 스택이 빌 때까지
-                revenue += (sell_cost - stack.pop())
+    for j in range(day_num-1, 0, -1):  # step을 음수로 지정하면 거꾸로 돌아감
+        if(day_cost[j] > max):
+            max = day_cost[j]
+        revenue += (max - day_cost[j])
 
     print("#{} {}".format(i+1, revenue))
+    # ???뭐가 문제지
