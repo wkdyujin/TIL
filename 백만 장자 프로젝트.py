476179dc8a6d2885286f7297ff466c5a6cd11a97
yujin <yujin1203713@gmail.com>
2021-07-17 23:45:26 +0900

    Algorithm

--- 백만 장자 프로젝트.py
+++ 백만 장자 프로젝트.py
@@ -5,12 +5,11 @@ for i in range(test_num):
     day_cost = list(map(int, input().split()))
 
     revenue = 0
-    max = day_cost[-1]  # 뒤에서부터 풀기! 큰값을 계속 갱신, 작으면 사서 큰값으로 팔음
+    max = day_cost[-1]
 
-    for j in range(day_num-1, 0, -1):  # step을 음수로 지정하면 거꾸로 돌아감
+    for j in range(day_num-2, -1, -1):
         if(day_cost[j] > max):
             max = day_cost[j]
         revenue += (max - day_cost[j])
 
     print("#{} {}".format(i+1, revenue))
-    # ???뭐가 문제지
