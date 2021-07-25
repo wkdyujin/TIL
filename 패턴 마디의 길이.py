507c4a8f7f1175470d604b112560bc2190112d6e
yujin <yujin1203713@gmail.com>
2021-07-25 13:34:08 +0900

    modify

--- 패턴 마디의 길이.py
+++ 패턴 마디의 길이.py
@@ -1,17 +1,10 @@
-# 패턴에서 반복되는 부분을 마디라고 부른다. 문자열을 입력 받아 마디의 길이를 출력하는 프로그램을 작성하라.
-# 1. 스택? xxx
-# 2. 앞에서부터 1~10번째 글자까지 잘라서 복사해놓고 비교? 최대 10번 반복함,
-
 T = int(input())
 for i in range(1, T + 1):
     s = input()
 
+    # 마디의 최대 길이가 10이므로 최대 10번 반복
     for j in range(1, 11):
-        split_s = s[:j]
-        dup = split_s*2
-
-        if(s[:len(dup)] == dup):
-            result = len(dup)//2
-            break
+        if(s[:j-1] == s[j:j*2-1]):
+            result = j
 
     print("#{} {}".format(i, result))
