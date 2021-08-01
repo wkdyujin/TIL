df0792d7c168e396b1beed115b295ab06aa27180
yujin <yujin1203713@gmail.com>
2021-08-01 15:31:23 +0900

    incomplete

--- 파스칼의 삼각형.py
+++ 파스칼의 삼각형.py
@@ -1,12 +1,20 @@
-# 어케해야하지
+# 재귀 !!...
+def print_line(i,):
+    if i == 0:
+        return 1
+    else:
+        return 1
+        return print_line(i)
+        return 1
+
 
 T = int(input())
 
 for test_case in range(1, T + 1):
     lenght = int(input())
-    top = 1
 
     for i in range(1, lenght+1):
         for j in range(i):
-            print(top, end='')
+            print_line(i, end='')
+
         print()
